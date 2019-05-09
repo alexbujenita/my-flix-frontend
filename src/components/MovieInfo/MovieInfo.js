40a5dc471de5900cc0fe685ae2710617a07dd052
Alex Bujenita <dekc89@gmail.com>
2019-05-09 21:57:46 +0100

    refactoring

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,132 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import YouTube from 'react-youtube';
-// import { withRouter } from 'react-router-dom';
 
-// import './MovieInfo.css'
-
-// import API from '../../API';
-// import ActorCard from '../ActorCard/ActorCard';
-
-
-// function MovieInfo({ match, userMovies, history }) {
-
-//     // user movies hook
-//     const setUserMovies = async () => {
-//         const userMovies = await API.getUserMovies(localStorage.getItem('token'));
-//         setUserMoviesTest(userMovies)
-//         return userMovies.includes(movie.id)
-//     }
-
-//     useEffect(() => {
-//         fetchMovie();
-//         fetchCast();
-//         fetchTrailer();
-//         setUserMovies();
-//     },[])
-
-//     window.scrollTo(0, 0)
-//     const [movie, setMovie] = useState({})
-//     const [cast, setCast] = useState([])
-//     const [trailer, setTrailer] = useState({})
-//     const [genres, setGenres] = useState([])
-//     const [belongs, setBelongs] = useState(false)
-//     const [userMoviesTest, setUserMoviesTest] = useState([])
-
-//     const fetchMovie = async () => {
-//         const movie = await API.getOneMovie(match.params.id)
-//         setGenres(movie.genres)
-//         setMovie(movie)
-//         belongsToUser();
-//         console.log("IN fetchMovie: " + userMoviesTest);
-//     }
-
-
-//     const fetchCast = async () => {
-//         const cast = await API.getMovieCredits(match.params.id)
-//         setCast(cast)
-//     }
-
-//     const fetchTrailer = async () => {
-//         const trailers = await API.getMovieTrailers(match.params.id)
-//         const trailer = await trailers.find(trail => trail.type === 'Trailer')
-//         setTrailer(trailer)
-//     }
-
-//     const { poster_path, original_title, overview, homepage, imdb_id } = movie
-
-//     const opts = {
-//         height: '390',
-//         width: '640',
-//         playerVars: {
-//             autoplay: 0
-//         }
-//     }
-
-//     const addMovieToCollection = () => {
-//         const token = localStorage.getItem('token')
-//         API.addMovieToCollection(movie, token)
-//             .then(history.push("/"))
-//     }
-
-//     // remove movie from collection
-//     const removeFavorite = (movie) => {
-//         console.log(movie);
-        
-//         API.removeMovieFromCollection(movie, localStorage.getItem('token'))
-//             .then(history.push("/"))
-//     }
-
-  
-//     // check user movie
-//     const belongsToUser = () => {
-        
-//         const userMoviesIds = userMovies.map(m => m.movie_ref_id)
-//         // return userMoviesIds.includes(movie.id)  
-//         const isIncluded = userMoviesIds.includes(movie.id)
-//         setBelongs(isIncluded)
-//         console.log(belongs)    
-//     }
-//     //
-
-//     return (
-//         <div className="show-movie">
-//             <div className="show-movie-details">
-//                 <img className='info-card-image' src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt='' />
-//                 <div className="movie-details">
-//                     <div className="movie-title">{original_title}</div>
-//                     <p onClick={() => belongsToUser(movie)}> {overview}</p>
-//                     <ul>
-//                         GENRES: {genres.map(genre => <li key={genre.id}> {genre.name}</li>)}
-//                     </ul>
-//                     <a href={homepage} target="_blank" rel="noopener noreferrer">MOVIE HOMEPAGE</a>
-//                     <br />
-//                     <a href={`https://www.imdb.com/title/${imdb_id}`} target="_blank" rel="noopener noreferrer">www.IMDB.com</a>
-//                     <div>
-                        
-//                     { belongs ? 
-//                     <button className="remove-button" onAbort={console.log(belongs)} onClick={() => removeFavorite(movie)} >REMOVE FROM COLLECTION</button>
-//                     :
-//                     <button className="add-button" onClick={addMovieToCollection} >ADD TO COLLECTION</button>}
-            
-//                     </div>
-//                     <YouTube
-//                         videoId={trailer ? trailer.key : '3cYBfuphkuE'}
-//                         opts={opts}
-//                     />
-//                 </div>
-//             </div>
-//             <h2>MOVIE CAST:</h2>
-//             <ul className="movie-cast">
-//                 {cast.map(actor => <ActorCard key={actor.cast_id} actor={actor} />)}
-//             </ul>
-//         </div>
-//     )
-
-
-
-// }
-
-// export default withRouter(MovieInfo)
 import React, { Component } from "react";
 import YouTube from "react-youtube";
 import { withRouter } from "react-router-dom";
@@ -136,7 +8,7 @@ import "./MovieInfo.css";
 import ActorCard from "../ActorCard/ActorCard";
 import API from "../../API";
 
-class MovieInfoState extends Component {
+class MovieInfo extends Component {
   state = {
     userMovies: [],
     movie: {},
@@ -166,7 +38,6 @@ class MovieInfoState extends Component {
 
   addMovieToCollection = () => {
     const { movie } = this.state;
-    const { history } = this.props;
     const token = localStorage.getItem("token");
     API.addMovieToCollection(movie, token).then(() => {
         API.getUserMovies(localStorage.getItem("token")).then(result => {
@@ -177,7 +48,6 @@ class MovieInfoState extends Component {
 
   removeMovieFromCollection = () => {
     const { movie } = this.state;
-    const { history } = this.props;
     const token = localStorage.getItem("token");
 
     API.removeMovieFromCollection(movie, token).then(() =>{
@@ -274,4 +144,4 @@ class MovieInfoState extends Component {
   }
 }
 
-export default withRouter(MovieInfoState);
+export default withRouter(MovieInfo);
