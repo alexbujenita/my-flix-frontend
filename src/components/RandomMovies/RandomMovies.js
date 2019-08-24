1749d187e689bad516c6173fa586b4289c3bc0dd
Alex Bujenita <dekc89@gmail.com>
2019-08-24 17:47:56 +0100

    MVP for functionality, src replaced for missing poster

--- src/components/RandomMovies/RandomMovies.js
+++ src/components/RandomMovies/RandomMovies.js
@@ -1,12 +1,14 @@
 import React, { PureComponent } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import API from "../../API";
 import MovieCard from "../MovieCard/MovieCard";
-import { mapping } from '../../mappings'
+import { mapping } from "../../mappings";
+import "./RandomMovies.css";
 
 class RandomMovies extends PureComponent {
   state = {
-    movies: []
+    movies: [],
+    disabled: false
   };
 
   async componentDidMount() {
@@ -14,23 +16,42 @@ class RandomMovies extends PureComponent {
     this.setState({ movies });
   }
 
-
-  renderMovies(movies) {
+  renderMovies = movies => {
     return movies.map(movie => {
-      return (<Link to={`/movie/${movie.id}`} key={movie.id}>
-        <MovieCard movie={movie} genres={mapping.genres} />
-      </Link>)
-    })
-  }
+      return (
+        <Link to={`/movie/${movie.id}`} key={movie.id}>
+          <MovieCard movie={movie} genres={mapping.genres} />
+        </Link>
+      );
+    });
+  };
+
+  moreRandomMovies = async () => {
+    this.setState({ disabled: true });
+    const movies = await API.getRandomMovies();
+    this.setState(
+      { movies: [...this.state.movies, ...movies] },
+      this.setState({ disabled: false })
+    );
+  };
 
   render() {
-    const { movies } = this.state;
+    const { movies, disabled } = this.state;
     return (
       <div className="movies-container">
         {movies && movies.length ? (
           this.renderMovies(movies)
         ) : (
           <h1 style={{ color: "white" }}>Randomly getting movies...</h1>
+        )}
+        {movies && movies.length && (
+          <button
+            className="moreButton"
+            disabled={disabled}
+            onClick={this.moreRandomMovies}
+          >
+            More!
+          </button>
         )}
       </div>
     );
