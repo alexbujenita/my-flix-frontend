47ec4bb7bdc701cb4bd375a0ccfee64b0cbad63f
Alex Bujenita <dekc89@gmail.com>
2019-10-25 21:54:20 +0100

    random movies connected

--- src/components/RandomMovies/RandomMovies.js
+++ src/components/RandomMovies/RandomMovies.js
@@ -1,23 +1,22 @@
 import React, { PureComponent } from "react";
 import { Link } from "react-router-dom";
-import API from "../../API";
 import MovieCard from "../MovieCard/MovieCard";
 import { mapping } from "../../mappings";
 import "./RandomMovies.css";
 
 import { connect } from "react-redux";
-import { FETCH_RANDOM_MOVIES } from '../../actions/randomMovies';
+import { FETCH_RANDOM_MOVIES } from "../../actions/randomMovies";
 
 class RandomMovies extends PureComponent {
   state = {
-    movies: [],
     disabled: false
   };
 
-  async componentDidMount() {
-    const movies = await API.getRandomMovies();
-    this.setState({ movies });
-    this.props.dispatch({ type: FETCH_RANDOM_MOVIES })
+  componentDidMount() {
+    const { randomMovies } = this.props;
+    if (!randomMovies.length) {
+      this.props.dispatch({ type: FETCH_RANDOM_MOVIES });
+    }
   }
 
   renderMovies = movies => {
@@ -30,26 +29,23 @@ class RandomMovies extends PureComponent {
     });
   };
 
-  moreRandomMovies = async () => {
+  moreRandomMovies = () => {
     this.setState({ disabled: true });
-    const movies = await API.getRandomMovies();
-    this.setState(
-      { movies: [...this.state.movies, ...movies] },
-      this.setState({ disabled: false })
-    );
+    this.props.dispatch({ type: FETCH_RANDOM_MOVIES });
+    setTimeout(() => this.setState({ disabled: false }), 4000);
   };
 
   render() {
-    const { movies, disabled } = this.state;
-    console.log(this.props)
+    const { disabled } = this.state;
+    const { randomMovies } = this.props;
     return (
       <div className="movies-container">
-        {movies && movies.length ? (
-          this.renderMovies(movies)
+        {randomMovies && randomMovies.length ? (
+          this.renderMovies(randomMovies)
         ) : (
           <h1 style={{ color: "white" }}>Randomly getting movies...</h1>
         )}
-        {movies && movies.length && (
+        {randomMovies && randomMovies.length && (
           <button
             className="moreButton"
             disabled={disabled}
@@ -66,7 +62,7 @@ class RandomMovies extends PureComponent {
 const mapStateToProps = ({ randomMovies }) => {
   return {
     randomMovies
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(RandomMovies);
