2682d7aac80ba242f50ebe38740ccf73e2e2879b
Alex Bujenita <dekc89@gmail.com>
2020-03-22 17:56:28 +0000

    testing lib for lazy loading images

--- src/components/ActorCard/ActorCard.js
+++ src/components/ActorCard/ActorCard.js
@@ -1,42 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LazyLoad from "react-lazyload";
 
 import "./ActorCard.css";
 
 const ActorCard = ({ actor }) => {
-  const { profile_path, name, character, id } = actor;
-
+  const { profile_path, name, character, id, gender } = actor;
+console.log(gender)
   return (
-    <div
-      key={id}
-      className="actor-card"
-    >
-      <div className="actor-card-inner">
-        <div className="actor-card-front">
-          <img
-          loading="lazy"
-            src={
-              profile_path
-                ? `https://image.tmdb.org/t/p/w500${profile_path}`
-                : "https://cdn.diversityavatars.com/assets/images/avatars-gallery/placeholder.png"
-            }
-            alt="Actor profile"
-          />
-        </div>
-        <div className="actor-card-back">
-          <Link
-            to={{
-              pathname: `/actor/${id}`,
-              state: { profilePhoto: profile_path }
-            }}
-            key={`${name}=${id}`}
-          >
-            <h1>{name}</h1>
-          </Link>
-          <h2>"{character}"</h2>
+    <LazyLoad height={100} width={145} once offset={30}>
+      <div key={id} className="actor-card">
+        <div className="actor-card-inner">
+          <div className="actor-card-front">
+            <img
+              loading="lazy"
+              src={
+                profile_path
+                  ? `https://image.tmdb.org/t/p/w500${profile_path}`
+                  : "https://cdn.diversityavatars.com/assets/images/avatars-gallery/placeholder.png"
+              }
+              alt="Actor profile"
+            />
+          </div>
+          <div className="actor-card-back">
+            <Link
+              to={{
+                pathname: `/actor/${id}`,
+                state: { profilePhoto: profile_path }
+              }}
+              key={`${name}=${id}`}
+            >
+              <h1>{name}</h1>
+            </Link>
+            <h2>"{character}"</h2>
+          </div>
         </div>
       </div>
-    </div>
+    </LazyLoad>
   );
 };
 
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,41 +1,44 @@
 import React, { PureComponent } from "react";
 import "./MovieCard.css";
+import LazyLoad from "react-lazyload";
 
 class MovieCard extends PureComponent {
   render() {
     const { movie, genres } = this.props;
     const { poster_path, title, release_date, genre_ids, overview } = movie;
     return (
-      <div className="movie-card">
-        <div className="card">
-          <div className="poster">
-            <img
-              className="card-image"
-              loading="lazy"
-              src={
-                poster_path
-                  ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                  : "http://wearetheranch.com/wp-content/uploads/2016/01/poster-placeholder.jpg"
-              }
-              alt="poster"
-            />
-          </div>
-          <div className="details">
-            <h2>{title}</h2>
-            <div className="release-date">Release date: {release_date}</div>
-            <div className="tags">
-              {genre_ids.map(genre_id => (
-                <span key={genre_id} className="genre">
-                  {genres.find(genre => genre.id === genre_id).name}
-                </span>
-              ))}
+      <LazyLoad height={400} width={250} once offset={1000}>
+        <div className="movie-card">
+          <div className="card">
+            <div className="poster">
+              <img
+                className="card-image"
+                loading="lazy"
+                src={
+                  poster_path
+                    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                    : "http://wearetheranch.com/wp-content/uploads/2016/01/poster-placeholder.jpg"
+                }
+                alt="poster"
+              />
             </div>
-            <div className="info">
-              <p>{overview}</p>
+            <div className="details">
+              <h2>{title}</h2>
+              <div className="release-date">Release date: {release_date}</div>
+              <div className="tags">
+                {genre_ids.map(genre_id => (
+                  <span key={genre_id} className="genre">
+                    {genres.find(genre => genre.id === genre_id).name}
+                  </span>
+                ))}
+              </div>
+              <div className="info">
+                <p>{overview}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </LazyLoad>
     );
   }
 }
